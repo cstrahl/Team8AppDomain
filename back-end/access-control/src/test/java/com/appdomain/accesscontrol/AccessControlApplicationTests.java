906a9a99d75048b13372694e10fe0d0a08fe7a81
cstrahl <christiandstrahl@gmail.com>
2020-01-15 15:16:54 -0500

    Initial Commit

---
+++ back-end/access-control/src/test/java/com/appdomain/accesscontrol/AccessControlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.appdomain.accesscontrol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccessControlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
